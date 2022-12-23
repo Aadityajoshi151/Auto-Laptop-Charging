18178b984d0770352be1d260a7dc653ff44df2ca
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-23 12:21:09 +0530

    add high and low conditions

--- src/alc.py
+++ src/alc.py
@@ -1,19 +1,40 @@
 import os
 import logging
-import datetime
 import tinytuya
 import psutil
 import schedule
 import time
 from dotenv import load_dotenv
+from datetime import datetime
 
 
-def checkStatus():
-    print(device_id)
-    print(device_ip_address)
-    print(local_key)
+def checkBatteryStatus():
+    battery_percentage = int(psutil.sensors_battery().percent)
+    plugged_in = psutil.sensors_battery().power_plugged
 
-#schedule.every(1).minutes.do(checkStatus)
+    if battery_percentage <= BATTERY_LOW_THRESHOLD and not plugged_in:
+        ttobj = tinytuya.OutletDevice(device_id, device_ip_address, local_key)
+        ttobj.set_version(3.3)
+        response = ttobj.turn_on()
+        if ENABLE_LOGGING:
+            if "Network Error: Device Unreachable" in response.values():
+                dtobj = datetime.now()             
+                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Smart plug not reachable.")
+            else:
+                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Charging started at {battery_percentage}% battery.")
+    
+    if battery_percentage >= BATTERY_HIGH_THRESHOLD and plugged_in:
+        ttobj = tinytuya.OutletDevice(device_id, device_ip_address, local_key)
+        ttobj.set_version(3.3)
+        response = ttobj.turn_off()
+        if ENABLE_LOGGING:
+            if "Network Error: Device Unreachable" in response.values():
+                dtobj = datetime.now()             
+                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Smart plug not reachable.")
+            else:
+                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Charging stopped at {battery_percentage}% battery.")
+
+#schedule.every(1).minutes.do(checkBatteryStatus)
 BATTERY_LOW_THRESHOLD=20
 BATTERY_HIGH_THRESHOLD=80
 ENABLE_LOGGING=True
@@ -26,7 +47,7 @@ device_id = os.getenv("DEVICE_ID")
 device_ip_address = os.getenv("DEVICE_IP_ADDRESS")
 local_key = os.getenv("LOCAL_KEY")
 
-schedule.every(5).seconds.do(checkStatus) ##TODO change seconds to minutes
+schedule.every(5).seconds.do(checkBatteryStatus) ##TODO change seconds to minutes
 
 def main():   
     while(True):
