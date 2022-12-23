8c74d8252feef81b0c4c5b95ae47eeafe854e38d
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-23 11:55:37 +0530

    get info from .env file

--- src/alc.py
+++ src/alc.py
@@ -9,14 +9,24 @@ from dotenv import load_dotenv
 
 
 def checkStatus():
-    print(int(psutil.sensors_battery().percent))
+    print(device_id)
+    print(device_ip_address)
+    print(local_key)
 
 #schedule.every(1).minutes.do(checkStatus)
 BATTERY_LOW_THRESHOLD=20
 BATTERY_HIGH_THRESHOLD=80
 ENABLE_LOGGING=True
+
 logging.basicConfig(filename='auto_laptop_charging_logs.log', filemode='a', format='%(message)s', level=logging.INFO)
-schedule.every(5).seconds.do(checkStatus)
+
+BASEDIR = os.path.abspath(os.path.dirname(__name__))
+load_dotenv(os.path.join(BASEDIR,"credentials.env"))
+device_id = os.getenv("DEVICE_ID")
+device_ip_address = os.getenv("DEVICE_IP_ADDRESS")
+local_key = os.getenv("LOCAL_KEY")
+
+schedule.every(5).seconds.do(checkStatus) ##TODO change seconds to minutes
 
 def main():   
     while(True):
