8f959860166511235d9b1ab83ebec5c8478f42cd
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-23 11:44:49 +0530

    set basic logging config

--- src/alc.py
+++ src/alc.py
@@ -7,14 +7,16 @@ import schedule
 import time
 from dotenv import load_dotenv
 
+
+def checkStatus():
+    print(int(psutil.sensors_battery().percent))
+
+#schedule.every(1).minutes.do(checkStatus)
 BATTERY_LOW_THRESHOLD=20
 BATTERY_HIGH_THRESHOLD=80
 ENABLE_LOGGING=True
-
-def checkStatus():
-    print(psutil.sensors_battery().percent)
-
-schedule.every(1).minutes.do(checkStatus)
+logging.basicConfig(filename='auto_laptop_charging_logs.log', filemode='a', format='%(message)s', level=logging.INFO)
+schedule.every(5).seconds.do(checkStatus)
 
 def main():   
     while(True):
