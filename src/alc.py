83cded7eb06d91ddeb2ac849e643549176df9cfc
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-23 12:52:03 +0530

    optimize code

--- src/alc.py
+++ src/alc.py
@@ -8,31 +8,29 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 
+def toggleStatus(state, logtext, battery_percent):
+    ttobj = tinytuya.OutletDevice(device_id, device_ip_address, local_key)
+    ttobj.set_version(3.3)
+    if state:
+        response = ttobj.turn_on()
+    else:
+        response = ttobj.turn_off()
+    if ENABLE_LOGGING:
+        dtobj = datetime.now()
+        if "Network Error: Device Unreachable" in response.values():
+            logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Smart plug not reachable.")
+        else:
+            logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Charging {logtext} at {battery_percent}% battery.")
+
 def checkBatteryStatus():
     battery_percentage = int(psutil.sensors_battery().percent)
     plugged_in = psutil.sensors_battery().power_plugged
 
     if battery_percentage <= BATTERY_LOW_THRESHOLD and not plugged_in:
-        ttobj = tinytuya.OutletDevice(device_id, device_ip_address, local_key)
-        ttobj.set_version(3.3)
-        response = ttobj.turn_on()
-        if ENABLE_LOGGING:
-            if "Network Error: Device Unreachable" in response.values():
-                dtobj = datetime.now()             
-                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Smart plug not reachable.")
-            else:
-                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Charging started at {battery_percentage}% battery.")
+        toggleStatus(True, "started", battery_percentage)
     
     if battery_percentage >= BATTERY_HIGH_THRESHOLD and plugged_in:
-        ttobj = tinytuya.OutletDevice(device_id, device_ip_address, local_key)
-        ttobj.set_version(3.3)
-        response = ttobj.turn_off()
-        if ENABLE_LOGGING:
-            if "Network Error: Device Unreachable" in response.values():
-                dtobj = datetime.now()             
-                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Smart plug not reachable.")
-            else:
-                logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Charging stopped at {battery_percentage}% battery.")
+        toggleStatus(False, "stopped", battery_percentage)
 
 BATTERY_LOW_THRESHOLD=20
 BATTERY_HIGH_THRESHOLD=80
@@ -46,7 +44,7 @@ device_id = os.getenv("DEVICE_ID")
 device_ip_address = os.getenv("DEVICE_IP_ADDRESS")
 local_key = os.getenv("LOCAL_KEY")
 
-schedule.every(7).minutes.do(checkBatteryStatus)
+schedule.every(5).minutes.do(checkBatteryStatus)
 
 def main():   
     while(True):
