ff657062330de25e5c5e1d5da2aa3252797732c1
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-23 12:24:08 +0530

    change seconds to minutes

--- src/alc.py
+++ src/alc.py
@@ -34,7 +34,6 @@ def checkBatteryStatus():
             else:
                 logging.info(f"[{dtobj.strftime('%d %b %Y - %I:%M:%S %p')}] Charging stopped at {battery_percentage}% battery.")
 
-#schedule.every(1).minutes.do(checkBatteryStatus)
 BATTERY_LOW_THRESHOLD=20
 BATTERY_HIGH_THRESHOLD=80
 ENABLE_LOGGING=True
@@ -47,7 +46,7 @@ device_id = os.getenv("DEVICE_ID")
 device_ip_address = os.getenv("DEVICE_IP_ADDRESS")
 local_key = os.getenv("LOCAL_KEY")
 
-schedule.every(5).seconds.do(checkBatteryStatus) ##TODO change seconds to minutes
+schedule.every(7).minutes.do(checkBatteryStatus)
 
 def main():   
     while(True):
