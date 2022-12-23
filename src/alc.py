d013ce2e8625312503cf6aa40b64ddb3f3531c2c
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-23 11:34:15 +0530

    print battery percent every minute

--- src/alc.py
+++ src/alc.py
@@ -1,15 +1,25 @@
 import os
 import logging
 import datetime
+import tinytuya
+import psutil
+import schedule
+import time
 from dotenv import load_dotenv
 
 BATTERY_LOW_THRESHOLD=20
 BATTERY_HIGH_THRESHOLD=80
 ENABLE_LOGGING=True
 
+def checkStatus():
+    print(psutil.sensors_battery().percent)
 
-def main():
-    print("Main Begins")
+schedule.every(1).minutes.do(checkStatus)
+
+def main():   
+    while(True):
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
